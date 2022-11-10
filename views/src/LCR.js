965af44d1be8d8190b76687b3c3154c4f3f5b979
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-11-10 12:56:51 +0200

    add link creation handling - change h1 to a in LRC.html - add 200 status xhr response handling

--- views/src/LCR.js
+++ views/src/LCR.js
@@ -1,5 +1,6 @@
 let button = document.querySelector('body > div.create_div > button');
 let text = document.querySelector('body > div.create_div > input.text-box')
+let link = document.querySelector('body > div.create_div > a');
 
 button.addEventListener('click', create)
 
@@ -8,13 +9,14 @@ async function create(){
     xhr.open("POST", '/v1/api/links/create', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify({
-        uqqwerl: text.value
+        url: text.value
     }));
 
    xhr.addEventListener('load', (event)=>{
         if(xhr.status){
-            //successful request
-            console.log(`${window.location.origin}/s/${JSON.parse(xhr.response).shortened}`)
+            link.style.visibility = 'visible';
+            link.href = `${window.location.origin}/s/${JSON.parse(xhr.response).shortened}`
+            link.innerHTML = `${window.location.origin}/s/${JSON.parse(xhr.response).shortened}`
         }else{
             //unsuccessful request
             console.log(xhr.status)
