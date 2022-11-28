b69157a153f1af1bc2104c48e9e7df988d1f91fc
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-11-29 00:54:54 +0200

    feat: email & password regex check fix: messages responses changed to plain text

--- routes/api/users/login.js
+++ routes/api/users/login.js
@@ -7,14 +7,14 @@ export default async function login(req, res){
         let user = await inspectUser(req.body.email)
 
         if(!user){
-            return res.status(400).json({error: 'email or password is incorrect'})
+            return res.status(400).send('Email or password is incorrect')
         }
 
         //Password check
         let isCorrect = await bcrypt.compare(req.body.password, user.password)
         
         if(isCorrect == false){
-            return res.status(400).json({error: 'email or password is incorrect'})
+            return res.status(400).send('Email or password is incorrect')
         }
 
         let token = jwt.sign({email: user.email, password: user.password}, process.env.JWT_SECRET, {algorithm: 'HS256', expiresIn: '1d'})
