482a4dba39cbcb3d3d597d1582910a890e3a91e1
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-11-30 16:19:42 +0200

    feat: authentication check api endpoint

--- routes/api/users/router.js
+++ routes/api/users/router.js
@@ -5,10 +5,13 @@ const router = express.Router()
 import register from './register.js'
 import login from './login.js'
 import token from './token.js'
+import authenticate from './auth.js';
+import validate from '../../middlewares/tokenValidate.js'
 
 router
     .post('/register', register)
     .post('/login', login)
     .post('/token', token)
+    .get('/auth', validate, authenticate)
 
 export default router;
