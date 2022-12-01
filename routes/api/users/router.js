fd9b425b274ca6631099485c09d4cfe070ac496a
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-12-01 21:16:43 +0200

    feat: API rate limit chore: add express-rate-limit package

--- routes/api/users/router.js
+++ routes/api/users/router.js
@@ -10,12 +10,13 @@ import feedback from './feedback.js'
 
 //middlewares
 import validate from '../../middlewares/tokenValidate.js'
+import { postLimiter } from '../../middlewares/rateLimiter.js'
 
 router
-    .post('/register', register)
-    .post('/login', login)
-    .post('/token', token)
-    .post('/feedback', feedback)
+    .post('/register', postLimiter, register)
+    .post('/login', postLimiter, login)
+    .post('/token', postLimiter, token)
+    .post('/feedback', postLimiter, feedback)
     .get('/auth', validate, authenticate)
 
 export default router;
