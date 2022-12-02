79d93a73ad670b3352ac2ef1a3e32c3bbd0bd3a5
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-12-02 19:34:31 +0200

    feat: HTTP test using SuperTest on Jest chore: add supertest, jest, @types/jest. add test script feat: split server and express app to different files

--- server.js
+++ server.js
@@ -1,31 +1,4 @@
-import express from 'express';
-import cookieParser from 'cookie-parser'
-const app = express();
+import app from './app.js';
 
-//.env config loading
-import {} from 'dotenv/config';
-
-//Middlewares
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({ extended: true }))
-
-// //Routes
-import v2 from './routes/v2.js';
-app.use('/v2', v2)
-
-//Source files
-app.use('/src', express.static('views/src'))
-
-//Views
-app.get('/', (req, res)=> res.sendFile(process.cwd() + '/views/landing.html'))
-app.get('/login', (req, res)=> res.sendFile(process.cwd() + '/views/login.html'))
-app.get('/register', (req, res)=> res.sendFile(process.cwd() + '/views/reg.html'))
-app.get('/try', (req, res)=> res.sendFile(process.cwd() + '/views/LCR.html'))
-app.get('/team', (req, res)=> res.sendFile(process.cwd() + '/views/team.html'))
-
-//Shortened links
-import redirect from './routes/redirect.js'
-app.get('/s/:url', redirect)
 
 app.listen(process.env.PORT || 3000, ()=> console.log('Server is listening on port:', process.env.PORT || 3000))
