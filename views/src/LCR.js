c049c7a470358cc21fd1b70885c5562660f76c5d
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-12-04 10:00:16 +0200

    feat: link creation page JS logic fix: styling fixes in the css

--- views/src/LCR.js
+++ views/src/LCR.js
@@ -1,25 +1,62 @@
-let button = document.querySelector('body > div.create_div > button');
-let text = document.querySelector('body > div.create_div > input.text-box')
-let link = document.querySelector('body > div.create_div > a');
-
-button.addEventListener('click', create)
+let button = document.querySelector('.create_button');
+let input = document.querySelector('.text-box')
+let links_group = document.querySelector('.links-group')
 
 async function create(){
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", '/v2/api/links/create', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-        url: text.value
-    }));
+    let response = await fetch(
+            '/v2/api/links/create',
+            {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({ url: input.value })
+            }
+            )
 
-   xhr.addEventListener('load', (event)=>{
-        if(xhr.status){
-            link.style.visibility = 'visible';
-            link.href = `${window.location.origin}/s/${JSON.parse(xhr.response).shortened}`
-            link.innerHTML = `${window.location.origin}/s/${JSON.parse(xhr.response).shortened}`
-        }else{
-            //unsuccessful request
-            console.log(xhr.status)
+    if(response.status == 200){
+        let body = await response.json()
+
+        if(document.querySelector('.error-message')){
+            document.querySelector('.error-message').remove()
         }
-   })
+        links_group.style.visibility = 'visible';
+
+        let container = document.createElement('div')
+        container.className = 'links-container'
+
+        let original_link = document.createElement('span')
+        original_link.className = 'original-link'
+        original_link.innerText = `${input.value.split('').splice(0, 30).join('')}...`
+
+        let shortened_link = document.createElement('span')
+        shortened_link.className = 'shortened-link'
+        shortened_link.innerText = `${window.location.origin}/s/${body.shortened}`
+        shortened_link.onclick = (x)=> {
+            navigator.clipboard.writeText(`${window.location.origin}/s/${body.shortened}`)
+        }
+
+        let monitor_button = document.createElement('a')
+        monitor_button.className = 'monitor-button'
+        monitor_button.href = `/monitor/${body.shortened}`
+        monitor_button.innerText = 'Monitor'
+
+
+        container.appendChild(original_link)
+        container.appendChild(shortened_link)
+        container.appendChild(monitor_button)
+
+        links_group.appendChild(container)
+    }else{
+        if(document.querySelector('.error-message')){
+            return
+        }
+
+        links_group.style.visibility = 'visible';
+
+        let message = document.createElement('p')
+        message.innerText = 'Please insert a valid link'
+        message.className = 'error-message'
+        message.style.color = 'red'
+
+        links_group.appendChild(message)
+    }
 }
